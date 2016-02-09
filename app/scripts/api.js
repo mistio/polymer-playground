56341f27a4965d23bfd029474c59cd38c51eca51
Fakiolas Marios <m.fakiolas@hotmail.gr>
2016-02-10 01:17:13 +0200

    scripts list / add script basic set up / edit script set up

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -8,5 +8,24 @@ var Mist = {
   }, {
     id: "kvm",
     isDefault: false
+  }],
+  scripts: [{
+    description: "",
+    entrypoint: "",
+    id: "448082e761c94660acfe54427c5ca461",
+    name: "test",
+    script: "#!/bin/bash\necho 'hello world '",
+    source: "inline",
+    type: "executable",
+    user: "marios@mist.io",
+  }, {
+    description: "test",
+    entrypoint: "",
+    id: "576bf98fe7e14d87b2b9075f099eeebe",
+    name: "runner",
+    script: "owner/repo",
+    source: "github",
+    type: "executable",
+    user: "marios@mist.io"
   }]
 };
