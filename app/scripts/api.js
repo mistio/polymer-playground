51672b5e0ec170dd76e18caa9fb01b109ca51d2f
Fakiolas Marios <m.fakiolas@hotmail.gr>
2016-02-12 10:13:02 +0200

    fix run script payload

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -8,7 +8,13 @@ var Mist = {
       id: "6f3e3c37-6ab3-40f2-9509-17d7d05e393d",
       name: "kvm-again",
       state: "running",
-      uuid: "b731a90760bf447e888997e3b55b9f179e8f238b"
+      uuid: "b731a90760bf447e888997e3b55b9f179e8f238b",
+      cloud: {
+        id: "29rGxvmKcwaRHkXwZkc2hByxcGmq",
+        provider: "packet",
+        title: "Packet.net",
+        enabled: true
+      }
     }]
   }, {
     id: "vYfxbj8HHbkFSGtE9AuZzqhCxFz",
@@ -19,7 +25,13 @@ var Mist = {
       id: "googlegr",
       name: "google.gr",
       state: "running",
-      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad3d"
+      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad3d",
+      cloud: {
+        id: "vYfxbj8HHbkFSGtE9AuZzqhCxFz",
+        provider: "bare_metal",
+        title: "google.gr",
+        enabled: true
+      }
     }]
   }, {
     id: "vYfxbj8HHbkFSGtE9AuZzqhCxF1",
@@ -30,7 +42,13 @@ var Mist = {
       id: "test",
       name: "test",
       state: "running",
-      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad3d"
+      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad3d",
+      cloud: {
+        id: "vYfxbj8HHbkFSGtE9AuZzqhCxF1",
+        provider: "bare_metal",
+        title: "test",
+        enabled: true
+      }
     }]
   }, {
     id: "vYfxbj8HHbkFSGtE9AuZzqhCxF2",
@@ -41,17 +59,35 @@ var Mist = {
       id: "jenkinsmaster",
       name: "jenkinsmaster",
       state: "running",
-      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad31"
+      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad31",
+      cloud: {
+        id: "vYfxbj8HHbkFSGtE9AuZzqhCxF2",
+        provider: "nephoscale",
+        title: "Nephoscale",
+        enabled: true
+      }
     }, {
       id: "echelon-mist-io",
       name: "echelon-mist-io",
       state: "running",
-      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad32"
+      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad32",
+      cloud: {
+        id: "vYfxbj8HHbkFSGtE9AuZzqhCxF2",
+        provider: "nephoscale",
+        title: "Nephoscale",
+        enabled: true
+      }
     }, {
       id: "openstackaki",
       name: "openstackaki",
       state: "running",
-      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad33"
+      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad33",
+      cloud: {
+        id: "vYfxbj8HHbkFSGtE9AuZzqhCxF2",
+        provider: "nephoscale",
+        title: "Nephoscale",
+        enabled: true
+      }
     }]
   }, {
     id: "vYfxbj8HHbkFSGtE9AuZzqhCxF3",
@@ -62,7 +98,13 @@ var Mist = {
       id: "i-4d7cbee1",
       name: "marios-monitoring",
       state: "running",
-      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad3d"
+      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad3d",
+      cloud: {
+        id: "vYfxbj8HHbkFSGtE9AuZzqhCxF3",
+        provider: "ec2_eu_west",
+        title: "EC2 Ireland",
+        enabled: true
+      }
     }]
   }, {
     id: "vYfxbj8HHbkFSGtE9AuZzqhCxF4",
@@ -73,12 +115,24 @@ var Mist = {
       id: "6f3e3c37-6ab3-40f2-9509-17d7d05e3931",
       name: "6f3e3c37-6ab3-40f2-9509-17d7d05e393d",
       state: "error",
-      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad31"
+      uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad31",
+      cloud: {
+        id: "vYfxbj8HHbkFSGtE9AuZzqhCxF4",
+        provider: "openstack",
+        title: "Openstack",
+        enabled: false
+      }
     }, {
       id: "6f3e3c37-6ab3-40f2-9509-17d7d05e3932",
       name: "demo",
       state: "stopped",
       uuid: "bfd05905e05b55ddaa120e8a0e3b8e954244ad32",
+      cloud: {
+        id: "vYfxbj8HHbkFSGtE9AuZzqhCxF4",
+        provider: "openstack",
+        title: "Openstack",
+        enabled: false
+      }
     }]
   }],
   keys: [{
